8ed72d962f336c3180ecf00fc49800a0530097d8
Sword Wen <wenjian_1107@hotmail.com>
2015-04-30 22:36:29 +0800

    support static files

--- django_demo/django_demo/settings.py
+++ django_demo/django_demo/settings.py
@@ -100,3 +100,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+#STATIC_ROOT =os.path.join( BASE_DIR , 'django_demo/static').replace('\\','/')
+#print "STATIC_ROOT:" + STATIC_ROOT
+STATICFILES_DIRS = (
+    os.path.join( BASE_DIR , 'django_demo/static').replace('\\','/'),
+)
--- django_demo/django_demo/urls.py
+++ django_demo/django_demo/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+
+from django.conf import settings
+
+from . import hello
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'django_demo.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', hello.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
-]
+] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT )
